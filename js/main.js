1caad471e6e92d8d9538d00bf16fead9d575f5ae
master-spectra <63547718+master-spectra@users.noreply.github.com>
2020-09-08 12:00:19 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', function() {
+	'use strict';
+
+	// jquery 
+	$('.carusel').slick();
+
+	let phone 		= document.querySelector('span'),
+		layout		= document.querySelector('.layout'), 
+		closeWindow	= document.querySelector('.btn-close-window'),
+		modalNumber	= document.querySelector('.numbers-modal-window');
+
+	phone.addEventListener('click', function() {
+		layout.style.display 			= "block";
+		modalNumber.style.transform 	= "translate(0 ,0)";
+
+		setTimeout(function() {
+			modalNumber.style.opacity 	= 1;
+		}, 302);
+	});
+
+	closeWindow.addEventListener('click', function() {
+		layout.style.display 				= "none";
+		modalNumber.style.opacity 			= 0;
+
+		setTimeout(function() {
+			modalNumber.style.transform 	= "translate(0%, -200%)";
+		}, 302);
+	});
+});
