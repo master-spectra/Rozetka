53296e205672f94f4d273c43602721a8b2ad9d41
master-spectra <63547718+master-spectra@users.noreply.github.com>
2020-09-10 21:08:15 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -22,20 +22,42 @@ window.addEventListener('DOMContentLoaded', function() {
 				fieldSignIn				= document.querySelectorAll('.field-sign-in'),
 				fieldSignUp				= document.querySelectorAll('.field-sign-up'),
 				warningMassegeSignIn	= document.querySelector('.warning-messege-sign-in'),
+				tabs 					= document.querySelector('.tabs'),
+				tab 					= document.querySelectorAll('.tab'),
+				tabConteiner 			= document.querySelectorAll('.tab-conteiner'),
 				warningMassegeSignUp	= document.querySelector('.warning-messege-sign-up'),
 				inputCity 				= document.querySelector('.city-input'),
 				btnSignHeader			= document.querySelector('.sign'),
 				applySetting			= document.querySelector('.ready'),
 				closeWindow				= document.querySelectorAll('.btn-close-window'),
+				btnShowCatalog 			= document.querySelector('.btn-show-catalog'),
 				signInBtn				= document.querySelectorAll('.sign-in-button'),
+				catalogMenu				= document.querySelector('.catalog-menu'),
+				windows 				= document.querySelectorAll('.window'),
 				signUpBtn				= document.querySelectorAll('.sign-up-button'),
 				signInWindowHeader		= document.querySelector('.sign-in-window'),
 				signUpWindow			= document.querySelector('.sign-up-window'),
+				contentPage				= document.querySelector('.content-page'),
 				modalCity 				= document.querySelector('.select-city-window'),
 				modalNumber				= document.querySelector('.numbers-modal-window');
 
 			// добавляем тех.переменную
 			let t;
+
+			document.addEventListener('click', function(e) {
+				windows.forEach(function(elem) { // перебираем массив с модальными окнами
+					if (e.target && getComputedStyle(elem).opacity == 1 && e.target != elem) { // пишим условие которое проверяет на какой объект мы нажали
+						layout.style.display = "none"; // скрываем layout
+						layout.classList.remove('catalog-mode'); // удаляем класс catalog-menu
+						contentPage.classList.remove('special'); // удаляем класс special
+						elem.style.opacity = "0"; // скрываем каждое окно
+
+						setTimeout(function() {
+							elem.style.transform = "translate(0, -200%)"; // выносим за экран	
+						}, 400); // задержка в 400мс
+					}
+				});
+ 			});
 
 			phone.addEventListener('click', function() {
 				layout.style.display 			= "block"; // делаем видимым
@@ -46,22 +68,18 @@ window.addEventListener('DOMContentLoaded', function() {
 				}, 302); // делаем задержку в 302мс
 			});
 
-			closeWindow.forEach(function(elem) {
-				elem.addEventListener('click', function() {
-					// скрываем элементы
-					layout.style.display 				= "none"; 
-					modalNumber.style.opacity 			= '0';
-					modalCity.style.opacity 			= '0';
-					signInWindowHeader.style.opacity 	= "0";
-					signUpWindow.style.opacity 			= "0";
+			closeWindow.forEach(function(elem) { // получаем каждую кнопку из массива 
+				elem.addEventListener('click', function() { // присваеваем к кнопке событие 
+					windows.forEach(function(item) { // перебираем массив с модальными окнами
+						layout.style.display = "none"; // скрываем layout
+						layout.classList.remove('catalog-mode'); // удаляем класс catalog-menu
+						contentPage.classList.remove('special'); // удаляем класс special
+						item.style.opacity = "0"; // скрываем каждое окно
 
-					setTimeout(function() {
-						// выносим за экран элементы
-						modalNumber.style.transform 		= "translate(0%, -200%)"; 	
-						modalCity.style.transform 			= "translate(0, -200%)"; 	
-						signInWindowHeader.style.transform 	= "translate(0, -200%)";
-						signUpWindow.style.transform 		= "translate(0, -200%)";
-					}, 302); // делаем задержку в 302мс
+						setTimeout(function() {
+							item.style.transform = "translate(0, -200%)"; // выносим за экран	
+						}, 400); // задержка в 400мс
+					});
 				});
 			});
 
@@ -151,7 +169,7 @@ window.addEventListener('DOMContentLoaded', function() {
 				fieldSignUp.forEach(function(elem) { // перебираем массив
 					if (elem.value.length < 7) { // если меньше 7 символов то выбьет предупреждение 
 						warningMassegeSignUp.style.display 		= "block"; // делаем видимым 
-						warningMassegeSignUp.textContent 		= 'Введите 8 или более символов'; // добавляем текст в warning
+						warningMassegeSignUp.textContent 		= 'Введите 4 или более символов'; // добавляем текст в warning
 					} else {
 						layout.style.display 						= "none"; // делаем видимым элемент
 						signUpWindow.style.opacity 					= "0"; // скрываем элемент
@@ -172,8 +190,45 @@ window.addEventListener('DOMContentLoaded', function() {
 					signUpWindow.style.transform 			= "translate(0, -200%)"; // выносим за экран
 				}, 302); // задержка в 302мс
 			});
+
+			btnShowCatalog.addEventListener('click', function() {
+				if (getComputedStyle(catalogMenu).opacity == "1") {
+					contentPage.classList.remove('special');
+					layout.classList.remove('catalog-mode');
+					catalogMenu.style.opacity = "0";
+					
+					setTimeout(function() {
+						catalogMenu.style.transform = "translate(0, -200%)"
+					}, 510);
+				} else {
+					contentPage.classList.add('special');
+					layout.classList.add('catalog-mode');
+					catalogMenu.style.transform = "translate(0, 0%)"
+
+					setTimeout(function() {
+						catalogMenu.style.opacity = "1";
+					}, 400);
+				};
+			});
+
+
+			tabs.addEventListener('mouseover', function(e) {
+				if (e.target && e.target.classList.contains('tab')) {
+					let target = e.target; // получаем элемент на который нажали
+
+					for (let i = 0; i < tab.length; i++) { // перебираем массив с табам
+						if (target == tab[i]) { // если элемент на которы нажали совпадает с тем который есть в массив...
+							tabConteiner.forEach(function(elem) {   // перебираю массив с tabConteiner
+								elem.classList.remove('active');	// удаляю класс active
+							});
+
+							tabConteiner[i].classList.add('active'); // добавляю класс active элементу который мы вызвали
+						}
+					}
+				}
+			});
 		},
 	};
 
-	Data.generalFuntion();
+	Data.generalFuntion(); // Вызываем функцию в объекте 
 });
